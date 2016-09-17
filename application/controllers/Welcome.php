b1bcbc6b6bf30033679a2426cbff88d86b52e864
peymantp <peymantparsa@gmail.com>
2016-09-16 19:39:47 -0700

    added random function and tested
    Signed-off-by: peymantp <peymantparsa@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -29,5 +29,23 @@ class Welcome extends Application
 
 		$this->render();
 	}
-
+	
+	public function random() 
+	{ 
+		// this is the view we want shown
+		$this->data['pagebody'] = 'homepage';
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array ();
+		foreach ($source as $record)
+		{
+			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where'], 'what' => $record['what']);
+		}
+                
+		//select a single index from authors
+		$rand = array_rand($authors, 1); 
+		//show the random author
+		$this->data['authors'] = array($authors[$rand]);
+		$this->render();
+	}
 }
